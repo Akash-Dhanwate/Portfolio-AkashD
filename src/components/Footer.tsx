eee2982e08ff531fba83a619390f2ff545bccae8
Akash Dhanwate <learnaids2025@gmail.com>
2026-09-14 10:36:04 +0530

    change the footer coe e

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion type Variants} from "framer-motion";
+import { motion ,type Variants} from "framer-motion";
 import Link from "next/link";
 
 const GithubIcon = (props: React.SVGProps<SVGSVGElement>) => (
